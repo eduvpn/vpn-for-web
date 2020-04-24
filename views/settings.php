3a6cd8aad88f8800b5afcabe75f25086ec3c57ee
François Kooman <fkooman@tuxed.net>
2020-04-24 15:43:35 +0200

    some more text changes

--- views/settings.php
+++ views/settings.php
@@ -2,15 +2,15 @@
 <?php $this->start('content'); ?>
     <h2>Settings</h2>
     <hr>
-    <h3>Add Custom Server</h3>
-    <form class="center" method="post" action="addCustomServer">
+    <h3>Add Other Server</h3>
+    <form class="center" method="post" action="addOtherServer">
             <label>https:// <input type="text" name="serverName" placeholder="vpn.example.org"></label> 
             <button value="add" type="submit">Add</button>
     </form>
     <hr>
     <form method="post" action="saveSettings">
         <fieldset>
-            <label><input type="checkbox" name="forceTcp" <?=$forceTcp ? 'checked' : ''; ?>> Make VPN connect only using TCP</label> 
+            <label><input type="checkbox" name="forceTcp" <?=$forceTcp ? 'checked' : ''; ?>> Connect only using TCP</label> 
         </fieldset>
         <fieldset>
             <button value="apply" type="submit">Apply</button>
