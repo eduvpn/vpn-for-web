9d7647cc877677e34163102cc870702c7721b432
François Kooman <fkooman@tuxed.net>
2020-04-25 21:59:03 +0200

    more text for "Add Other Server"

--- views/add_other_server.php
+++ views/add_other_server.php
@@ -1,6 +1,9 @@
 <?php $this->layout('base'); ?>
 <?php $this->start('content'); ?>
     <h2>Add Other Server</h2>
+    <p>Here you can add your own VPN server. <strong>Only</strong> use this 
+function if your VPN server is not listed under "Connect to your Institute" or "Protect yourself Online"!</p>
+
     <form class="center" method="post" action="addOtherServer">
             <label>https:// <input type="text" name="serverName" placeholder="vpn.example.org"></label> 
             <button value="add" type="submit">Add</button>
