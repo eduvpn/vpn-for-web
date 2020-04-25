d05166fee803c7d8c5b7419825b7562c40c33a8d
François Kooman <fkooman@tuxed.net>
2020-04-25 21:56:50 +0200

    do some more styling

--- views/home.php
+++ views/home.php
@@ -1,14 +1,13 @@
 <?php $this->layout('base'); ?>
 <?php $this->start('content'); ?>
 
-<h2>Connect to your Institute</h2>
 <?php if (0 === count($myInstituteAccessServerList)): ?>
     <form class="home center" method="get" action="chooseServer">
-    <button>🏛️ Add your Institute...</button>
+    <button>🏛️ Connect to your Institute...</button>
     </form>
 <?php else: ?>
     <ul>
-        <form method="get" action="getProfileList">
+        <form class="home center" method="get" action="getProfileList">
 <?php  foreach ($myInstituteAccessServerList as $serverEntry): ?>
         <li>
             <button name="baseUri" value="<?=$this->e($serverEntry['base_uri']); ?>">🏛️ <?=$this->l($serverEntry['display_name']); ?></button>
@@ -19,29 +18,25 @@
     <div class="add"><a class="small" href="chooseServer">Add Another Institute...</a></div>
 <?php endif; ?>
 
-<h2>Protect Yourself Online</h2>
 <?php if (null === $secureInternetServerInfo): ?>
     <form class="home center" method="get" action="chooseIdP">
-    <button>🌍 Add a Location...</button>
+        <button>🌍 Protect yourself Online...</button>
     </form>
 <?php else: ?>
     <form class="home center" method="get" action="getProfileList">
-    <button name="baseUri" value="<?=$this->e($secureInternetServerInfo['base_uri']); ?>">🌍 <?=$this->l($secureInternetServerInfo['display_name']); ?></button>
+        <button name="baseUri" value="<?=$this->e($secureInternetServerInfo['base_uri']); ?>">🌍 <?=$this->l($secureInternetServerInfo['display_name']); ?></button>
     </form>
     <div class="add"><a class="small" href="switchLocation">Change Location...</a></div>
 <?php endif; ?>
 
-<?php if (0 !== count($myAlienServerList)): ?>
-    <h2>Other Servers</h2>
-    <ul>
-        <form method="get" action="getProfileList">
+<ul>
+    <form class="home center" method="get" action="getProfileList">
 <?php  foreach ($myAlienServerList as $serverEntry): ?>
-        <li>
-            <button name="baseUri" value="<?=$this->e($serverEntry['base_uri']); ?>">👽 <?=$this->l($serverEntry['display_name']); ?></button>
-        </li>
+    <li>
+        <button name="baseUri" value="<?=$this->e($serverEntry['base_uri']); ?>">👽 <?=$this->l($serverEntry['display_name']); ?></button>
+    </li>
 <?php endforeach; ?>
-        </form>
-    </ul>
-<?php endif; ?>
-<p class="center small"><a href="<?=$this->e($rootUri); ?>addOtherServer">Add Other Server...</a></p>
+    </form>
+</ul>
+<div class="add"><a class="small" href="<?=$this->e($rootUri); ?>addOtherServer">Add Other Server...</a></div>
 <?php $this->stop('content');
